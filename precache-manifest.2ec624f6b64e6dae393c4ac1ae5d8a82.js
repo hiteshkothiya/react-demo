4052993c71994c893f786f457fbc070d615b9f54
hiteshkothiya <hitkothiya@gmail.com>
2020-03-09 12:56:58 +0530

    Updates

---
+++ precache-manifest.2ec624f6b64e6dae393c4ac1ae5d8a82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3331d7094647de779a67012916e946c4",
+    "url": "/react-demo/index.html"
+  },
+  {
+    "revision": "f927cbf0a24870a8b1fc",
+    "url": "/react-demo/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "34415fea4eeb7ec4d937",
+    "url": "/react-demo/static/css/main.11c6b846.chunk.css"
+  },
+  {
+    "revision": "f927cbf0a24870a8b1fc",
+    "url": "/react-demo/static/js/2.c52f415d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-demo/static/js/2.c52f415d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "34415fea4eeb7ec4d937",
+    "url": "/react-demo/static/js/main.d5f8ca39.chunk.js"
+  },
+  {
+    "revision": "85aa3ec3aa7b0b1b1622",
+    "url": "/react-demo/static/js/runtime-main.5ca7d8c7.js"
+  }
+]);
--- src/components/Backdrop/Backdrop.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './_scss/_index.scss';
-
-const backDrop = props => (
-    <div className="Backdrop" onClick={props.click} />
-);
-
-export default backDrop;
